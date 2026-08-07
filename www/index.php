43868bdf126f7fbd42bb76831648e3afabccf074
LAC-git <98246849+LAC-git@users.noreply.github.com>
2026-08-07 09:16:32 -0300

    Adicionando arquivos iniciais: index.php, script-index.js e .gitignore.

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Loja Virtual</title>
+    <link rel="stylesheet" href="../styles/index.css" />
+  </head>
+  <body>
+    <main>
+      <h1>Loja Virtual no PHP</h1>
+      <p>Esse é um exemplo simples de uma loja virtual desenvolvida em PHP.</p>
+      <button id="actionButton">Clique aqui</button>
+      <p id="message"></p>
+    </main>
+    <script src="../js/script-index.js"></script>
+  </body>
+</html>

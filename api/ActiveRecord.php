741f28512d5a1196d5845e493acfc811a19e85a8
LAC-git <98246849+LAC-git@users.noreply.github.com>
2026-08-21 00:18:05 -0300

    Criando a estrutura dos active records

---
+++ api/ActiveRecord.php
@@ -0,0 +1,15 @@
+<?php
+
+// Precisa da conexão né
+require_once "DBconn.php";
+
+// Incluindo cada um dos active records
+require_once "Cliente.php";
+require_once "Funcionario.php";
+require_once "Pedido.php";
+require_once "Categoria.php";
+require_once "Produto.php";
+require_once "ItemProduto.php";
+
+?>
+
